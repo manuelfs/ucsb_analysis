c295ceea5f15c303637a43b5796c0f9a2d4aaf21
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2014-09-14 21:30:23 -0700

    Added isolation plots

--- macros/hltdistributions.C
+++ macros/hltdistributions.C
@@ -8,6 +8,7 @@
 #include "TH1F.h"
 #include "TCanvas.h"
 #include "TLegend.h"
+#include "TLine.h"
 #include "TString.h"
 
 #define NSam 6
@@ -20,42 +21,56 @@ void ReadChains(TChain *chain[], TString folder, TString LegNames[]);
 
 class hfeats {
 public:
-  hfeats(TString ivarname, int inbins, float iminx, float imaxx, TString ititle="", TString icuts="1"){
+  hfeats(TString ivarname, int inbins, float iminx, float imaxx, TString ititle="", TString icuts="1", 
+	 float icut=-1){
     varname = ivarname; nbins = inbins; minx = iminx; maxx = imaxx; title = ititle;
-    cuts = icuts;
-    tag = ivarname+"_"+cuts; tag.ReplaceAll("_1",""); 
+    cuts = icuts; cut = icut;
+    tag = ivarname+"_"+cuts; tag.ReplaceAll("_1",""); tag.ReplaceAll(".",""); 
     tag.ReplaceAll("(",""); tag.ReplaceAll("$","");  tag.ReplaceAll(")",""); 
+    tag.ReplaceAll("[",""); tag.ReplaceAll("]",""); 
     tag.ReplaceAll("/","_"); tag.ReplaceAll("*",""); tag.ReplaceAll("&&","_");
     tag.ReplaceAll(">",""); tag.ReplaceAll("<",""); tag.ReplaceAll("=","");
   }
   TString title, varname, tag, cuts;
   int nbins;
-  float minx, maxx;
+  float minx, maxx, cut;
 };
 
-void hltdistributions(TString folder="root/hlt/ht200/") { 
+void hltdistributions(TString folder="root/hlt/mu15noiso") { 
   styles style("Standard"); style.nDivisions = 708; style.setDefaultStyle();
   vector<hfeats> vars;
   TCanvas can;
+  int inilep(0), endlep(1);
 
-  //vars.push_back(hfeats("mindr_mu",30,0,3,"Minimum #DeltaR(#mu,jet)","onht>500"));
-  //vars.push_back(hfeats("mindr2_mu",25,0,5,"2nd minimum #DeltaR(#mu,jet)","onht>500"));
-  vars.push_back(hfeats("Sum$(jets_pt>40)",15,-0.5,14.5,"Number of jets with p_{T} > 40 GeV","onht>500"));
-  //vars.push_back(hfeats("Sum$(jets_pt>60)",15,-0.5,14.5,"Number of jets with p_{T} > 60 GeV","onht>500"));
-  // vars.push_back(hfeats("Sum$(jets_pt>40)",15,-0.5,14.5,"Number of jets with p_{T} > 40 GeV"));
-  // vars.push_back(hfeats("Sum$(jets_pt>60)",15,-0.5,14.5,"Number of jets with p_{T} > 60 GeV"));
-  // vars.push_back(hfeats("onht",50,0,2500,"HLT H_{T} (GeV)"));
-  // vars.push_back(hfeats("onmet",50,0,750,"HLT MET (GeV)"));
-  // vars.push_back(hfeats("Max$(mus_pt)",29,5,150,"Leading #mu p_{T} (GeV)"));
+   vars.push_back(hfeats("onht",50,0,2500,"HLT H_{T} (GeV)"));
+   vars.push_back(hfeats("onmet",50,0,750,"HLT MET (GeV)"));
+  // vars.push_back(hfeats("onht",50,0,2500,"HLT H_{T} (GeV)","Sum$(jets_pt>40)>=5"));
+  // vars.push_back(hfeats("onmet",50,0,750,"HLT MET (GeV)","Sum$(jets_pt>40)>=5"));
+  //vars.push_back(hfeats("mindr_mu",25,0,2.5,"Minimum #DeltaR(#mu,jet)"));
+  //vars.push_back(hfeats("mindr2_mu",30,0,6,"2nd minimum #DeltaR(#mu,jet)","Sum$(jets_pt>40)>=5"));
+  //vars.push_back(hfeats("abs(jets_phi[0]-jets_phi[1])*(abs(jets_phi[0]-jets_phi[1])<3.1416)+(2*3.1416-abs(jets_phi[0]-jets_phi[1]))*(abs(jets_phi[0]-jets_phi[1])>=3.1416)",63,0,3.15,"#Delta#phi(jet1,jet2)"));
+   // vars.push_back(hfeats("Sum$(jets_pt>40)",16,-0.5,15.5,"Number of jets with p_{T} > 40 GeV","onht>500"));
+   // vars.push_back(hfeats("Sum$(jets_pt>60)",16,-0.5,15.5,"Number of jets with p_{T} > 60 GeV","onht>500"));
+   // vars.push_back(hfeats("Sum$(jets_pt>40)",16,-0.5,15.5,"Number of jets with p_{T} > 40 GeV"));
+   // vars.push_back(hfeats("Sum$(jets_pt>60)",16,-0.5,15.5,"Number of jets with p_{T} > 60 GeV"));
+  // vars.push_back(hfeats("Max$(mus_pt)",29,5,150,"Leading #mu HLT p_{T} (GeV)"));
+  // vars.push_back(hfeats("Max$(mus_pt)",29,5,150,"Leading #mu HLT p_{T} (GeV)","onht>500"));
+  // vars.push_back(hfeats("Max$(genmus_pt)",29,5,150,"Leading #mu gen p_{T} (GeV)"));
+  //  vars.push_back(hfeats("Max$(lep_pt*(abs(lep_id)==13&&lep_reliso<0.12))",29,5,150,"Leading #mu reco p_{T} (GeV)",
+  //   			"abs(lep_id)==13&&lep_reliso<0.12"));
+
   // vars.push_back(hfeats("Max$(els_pt)",29,5,150,"Leading e p_{T} (GeV)"));
-  // vars.push_back(hfeats("Max$(mus_pt)",29,5,150,"Leading #mu p_{T} (GeV)","onht>500&&onmet>50"));
   // vars.push_back(hfeats("Max$(els_pt)",29,5,150,"Leading e p_{T} (GeV)","onht>500&&onmet>50"));
-  // vars.push_back(hfeats("Max$(genmus_pt)",29,5,150,"Leading gen #mu p_{T} (GeV)","onht>500&&onmet>50"));
-  // vars.push_back(hfeats("Max$(genels_pt)",29,5,150,"Leading gen e p_{T} (GeV)","onht>500&&onmet>50"));
-  // vars.push_back(hfeats("Max$(lep_pt*(abs(lep_id)==13))",29,5,150,"Leading reco #mu p_{T} (GeV)",
-  //  			"onht>500&&onmet>50&&abs(lep_id)==13"));
-  // vars.push_back(hfeats("Max$(lep_pt*(abs(lep_id)==11))",29,5,150,"Leading reco e p_{T} (GeV)",
-  //  			"onht>500&&onmet>50&&abs(lep_id)==11"));
+  // vars.push_back(hfeats("Max$(genels_pt)",29,5,150,"Leading gen e p_{T} (GeV)"));
+  // vars.push_back(hfeats("Max$(lep_pt*(abs(lep_id)==11&&lep_reliso<0.15))",29,5,150,"Leading e reco p_{T} (GeV)",
+  //  			"abs(lep_id)==11&&lep_reliso<0.15"));
+
+  // vars.push_back(hfeats("Min$(els_trackiso+(els_pt<15)*10000)",26,0,0.26,"Electron track relative isolation","1",0.05));
+  // vars.push_back(hfeats("Min$(els_ecaliso+(els_pt<15)*10000)",51,0,1.02,"Electron ECAL relative isolation","1",0.2));
+  // vars.push_back(hfeats("Min$(els_hcaliso+(els_pt<15)*10000)",56,0,0.56,"Electron HCAL relative isolation","1",0.11));
+  // vars.push_back(hfeats("Min$(els_reliso+(els_pt<15)*10000)",50,0,1.7,"Electron total relative isolation","1",0.36));
+  
+  vars.push_back(hfeats("Min$(mus_reliso+(mus_pt<15)*10000)",51,0,1.02,"Muon total relative isolation","1",0.15));
 
   vector<int> indchain;
   indchain.push_back(0); //(1025,625)
@@ -70,22 +85,30 @@ void hltdistributions(TString folder="root/hlt/ht200/") {
   TLegend leg(legX, legY-legH, legX+legW, legY);
   leg.SetTextSize(0.056); leg.SetFillColor(0); leg.SetFillStyle(0); leg.SetBorderSize(0);
   leg.SetTextFont(132);
-  int hcolors[] = {28,8,6,1,4,2};
+
+  TLine line; line.SetLineColor(28); line.SetLineWidth(2); line.SetLineStyle(2);
+
+  int hcolors[] = {8,28,6,1,4,2};
   int hstyles[] = {2,2,2,1,1,1};
   int hwidths[] = {4,4,4,2,2,2};
   vector<TH1F*> histo[2][NSam];
   TChain *chain[NSam];
   TString lepfolders[] = {"el15", "mu15"}, currentfolder, legnames[NSam];
   TString hname, pname, variable, leghisto, totcut, title;
-  for(int lep(0); lep<2; lep++){
-    currentfolder = folder + lepfolders[lep];
+  if(folder.Contains("mu15")) {inilep = 1; endlep = 2;}
+  for(int lep(inilep); lep<endlep; lep++){
+    currentfolder = folder;// + lepfolders[lep];
     ReadChains(chain, currentfolder, legnames);
     for(unsigned var(0); var<vars.size(); var++){
-      if(vars[var].varname.Contains("el") && !lepfolders[lep].Contains("el")) continue;
-      if(vars[var].varname.Contains("mu") && !lepfolders[lep].Contains("mu")) continue;
+      if((vars[var].varname.Contains("els")||vars[var].varname.Contains("11")) 
+	 && !lepfolders[lep].Contains("el")) continue;
+      if((vars[var].varname.Contains("mus")||vars[var].varname.Contains("13"))  
+	 && !lepfolders[lep].Contains("mu")) continue;
 
       title = vars[var].cuts; if(title=="1") title = "";
-      title.ReplaceAll(">", " > "); title.ReplaceAll("&&", ", "); 
+      title.ReplaceAll("els_pt","p^{e}_{T}");title.ReplaceAll("mus_pt","p^{#mu}_{T}");
+      title.ReplaceAll("Sum$(jets_pt>40)","n_{jets}^{40}");title.ReplaceAll("abs(lep_id)==13&&","");
+      title.ReplaceAll(">=", " #geq "); title.ReplaceAll(">", " > "); title.ReplaceAll("&&", ", "); 
       title.ReplaceAll("onmet", "MET"); title.ReplaceAll("onht", "H_{T}"); 
       leg.Clear();
       float maxhisto(-999);
@@ -97,12 +120,21 @@ void hltdistributions(TString folder="root/hlt/ht200/") {
 	histo[lep][var][sam]->SetLineWidth(hwidths[indchain[sam]]);
 	totcut = "weight*("+vars[var].cuts+")";
 	variable = vars[var].varname;
+	if(variable.Contains("iso") && indchain[sam]==0){
+	  if(lepfolders[lep].Contains("el")) totcut = "weight*wl1ht200*(Max$(genels_pt[0])>0&&"+vars[var].cuts+")";
+	  else totcut = "weight*wl1ht200*(Max$(genmus_pt[0])>0&&"+vars[var].cuts+")";
+	  cout<<totcut<<endl;
+	}
 	if(indchain[sam]==1||indchain[sam]==2||indchain[sam]==5){
 	  if(variable == "Max$(mus_pt)") variable = "onmupt";
 	  if(variable == "Max$(els_pt)") variable = "onelpt";
 	}
 	chain[indchain[sam]]->Project(hname, variable, totcut);
-	histo[lep][var][sam]->Scale(100./histo[lep][var][sam]->Integral());
+	histo[lep][var][sam]->SetBinContent(vars[var].nbins,
+					    histo[lep][var][sam]->GetBinContent(vars[var].nbins)+
+					    histo[lep][var][sam]->GetBinContent(vars[var].nbins+1));
+	if(legnames[indchain[sam]].Contains("QCD")) histo[lep][var][sam]->Scale(100./histo[lep][var][sam]->Integral());
+	else histo[lep][var][sam]->Scale(100./histo[lep][var][sam]->Integral());
 	if(sam==0){
 	  histo[lep][var][sam]->SetXTitle(vars[var].title);
 	  histo[lep][var][sam]->SetYTitle("Entries (%)");
@@ -110,20 +142,23 @@ void hltdistributions(TString folder="root/hlt/ht200/") {
 	} else histo[lep][var][sam]->Draw("same");
 	if(maxhisto < histo[lep][var][sam]->GetMaximum()) maxhisto = histo[lep][var][sam]->GetMaximum();
 	leghisto = legnames[indchain[sam]]+" [#mu = ";
-	leghisto += RoundNumber(histo[lep][var][sam]->GetMean(),1) + "]";
+	leghisto += RoundNumber(histo[lep][var][sam]->GetMean(),2) + "]";
 	leg.AddEntry(histo[lep][var][sam], leghisto);
       } // Loop over samples
       histo[lep][var][0]->SetMaximum(maxhisto*1.1);
-      leg.Draw();
+      leg.Draw(); 
+      if(vars[var].cut>0) line.DrawLine(vars[var].cut, 0, vars[var].cut, maxhisto*1.1);
       pname = "plots/hltstudy/"+lepfolders[lep]+"_"+vars[var].tag+".eps";
       can.SaveAs(pname);
     }// Loop over variables
   }// Loop over el15 and mu15
 
-  for(int lep(0); lep<2; lep++)
+  for(int lep(inilep); lep<endlep; lep++)
     for(unsigned var(0); var<vars.size(); var++){
-      if(vars[var].varname.Contains("el") && !lepfolders[lep].Contains("el")) continue;
-      if(vars[var].varname.Contains("mu") && !lepfolders[lep].Contains("mu")) continue;
+      if((vars[var].varname.Contains("els")||vars[var].varname.Contains("11")) 
+	 && !lepfolders[lep].Contains("el")) continue;
+      if((vars[var].varname.Contains("mus")||vars[var].varname.Contains("13"))  
+	 && !lepfolders[lep].Contains("mu")) continue;
       for(unsigned sam(0); sam < indchain.size(); sam++)
 	if(histo[lep][var][sam]) histo[lep][var][sam]->Delete();
     }
@@ -136,13 +171,13 @@ void ReadChains(TChain *chain[], TString folder, TString LegNames[]){
      folder+"/*T1tttt*1500_*", 
      folder+"/*QCD*",
      folder+"/*TT*",
-     folder+"/*WJets*"};
+     folder+"/W*"};
   LegNames[0] = "T1tttt(1025,625)";
   LegNames[1] = "T1tttt(1200,800)";
   LegNames[2] = "T1tttt(1500,100)";
   LegNames[3] = "QCD";
   LegNames[4] = "t#bar{t}";
-  LegNames[5] = "W+jets";
+  LegNames[5] = "W#rightarrowl#nu";
 
   for(int sam(0); sam < NSam; sam++){
     chain[sam] = new TChain("tree");
--- macros/hltstudy.C
+++ macros/hltstudy.C
@@ -25,9 +25,8 @@
 #include "TH2F.h"
 #include "TMath.h"
 
-#define NVar 2
 #define NSam 6
-#define NLeps 1
+#define NCuts 3
 
 using namespace std;
 using std::cout;
@@ -35,8 +34,8 @@ using std::endl;
 
 void ReadChains(TChain *chain[], int entries[], TString folder);
 
-void hltstudy(TString folder="root/hlt/ht200/mu15/", TString filetype = ".eps"){
-  styles style("2Dtitle"); style.setDefaultStyle();
+void hltstudy(TString folder="root/hlt/el15noiso/", TString addtitle = ""){
+  styles style("2Dtitle"); style.setDefaultStyle(); gStyle->SetPaintTextFormat("4.1f");
   gStyle->SetHatchesLineWidth(2);
   TCanvas can;
   bool isEl = folder.Contains("el")?true:false;
@@ -58,12 +57,19 @@ void hltstudy(TString folder="root/hlt/ht200/mu15/", TString filetype = ".eps"){
   TString xTitle = "Minimum HLT PF H_{T} (GeV)";
   TString yTitle = "Minimum HLT PF E_{T,miss} (GeV)";
   TString zTitle = "HLT rate (Hz)";
-  TH2F *hRate[NLeps][NSam+1], *hContour[NLeps][4];
-  //TString Cuts[] = {"onmupt>15.0", "onmupt>20.0", "onmupt>20.0"};
-  TString Cuts[] = {"1", "1", "1"};
-  //TString Cuts[] = {"((abs(jets_phi[0]-jets_phi[1])*(abs(jets_phi[0]-jets_phi[1])<3.1416)+(2*3.1416-abs(jets_phi[0]-jets_phi[1]))*(abs(jets_phi[0]-jets_phi[1])>=3.1416))>2.8)", "1", "1"};
-  int nBinsHt = 100, nBinsMet = 90;
-  float minHt=200, maxHt=1000, minMet=0, maxMet=180;
+  TH2F *hRate[NCuts][NSam+1], *hContour[NCuts][4];
+
+  TString eliso5x = "Max$(els_pt*(els_trackiso<0.25&&(els_eta<1.479&&els_ecaliso<1||els_eta>=1.479&&els_ecaliso<0.75)&&els_hcaliso<0.55))>15";
+  TString muiso5x = "Max$(mus_pt*(mus_reliso<1))>15";
+  TString Cuts[] = {"1", "iso5x", "iso5x&&Max$(bjets_csv*(bjets_pt>40))>0.7"};
+  TString Tags[] = {"noiso", "iso5x", "iso5xbtag"};
+  TString TitleCuts[] = {"", ", RelIso < iso5x", ", RelIso < iso5x, CSV > 0.7"};
+  TString tagfolder = folder;
+  if(tagfolder[tagfolder.Sizeof()-2] == '/') tagfolder.Remove(tagfolder.Sizeof()-2);
+  tagfolder.Remove(0,tagfolder.Last('/')+1);
+
+  int nBinsHt = 14, nBinsMet = 14;
+  float minHt=200, maxHt=900, minMet=0, maxMet=140;
 
   double legX = 0.72, legY = 0.87;
   double legW = 0.12, legH = 0.2;
@@ -73,10 +79,10 @@ void hltstudy(TString folder="root/hlt/ht200/mu15/", TString filetype = ".eps"){
   vector<double> levels, htlevels;
   levels.push_back(1); levels.push_back(2); 
   levels.push_back(5); levels.push_back(10); 
-  htlevels.push_back(400); htlevels.push_back(500); 
-  htlevels.push_back(600); htlevels.push_back(700); 
-  int lcolors[]={2, 28, 1, 4};
-  TString styles[]={"cont3 same", "cont3 same", "cont3 same", "cont3 same"};
+  // htlevels.push_back(400); htlevels.push_back(500); 
+  // htlevels.push_back(600); htlevels.push_back(700); 
+  //int lcolors[]={2, 28, 1, 4};
+  TString styles[]={"cont3 same", "cont3 same", "cont3 same", "cont3 same"}, Title;
   vector<TString>legtag;
   for(unsigned tag(0); tag < levels.size(); tag++){
     legtag.push_back(""); 
@@ -85,30 +91,49 @@ void hltstudy(TString folder="root/hlt/ht200/mu15/", TString filetype = ".eps"){
     legtag[tag] += " Hz";
   }
   
-  TString Title = "p_{T}^{#mu} > 15 GeV";
-  if(isEl) Title.ReplaceAll("#mu","e");
-  for(int lep(0); lep < NLeps; lep++){
-    totCut = "1.4e-2/19600*weight*(" + Cuts[lep] + ")";
+  for(int icut(0); icut < NCuts; icut++){
+    totCut = "1.4e-2/19600*weight*(" + Cuts[icut] + ")";
+    Title = "p_{T}^{#mu} > 15 GeV"+TitleCuts[icut];
+    if(isEl) {
+      Title.ReplaceAll("#mu","e");
+      Title.ReplaceAll("iso5x", "1.7");
+      totCut.ReplaceAll("iso5x", eliso5x);
+    } else {
+      totCut.ReplaceAll("iso5x", muiso5x);
+      Title.ReplaceAll("iso5x", "1");
+    }
+    if(addtitle!="") Title += (", "+addtitle);
+    cout<<endl<<"Doing cuts "<<totCut<<endl;
     for(int tag(0); tag < 4; tag++){
-      Hname = Htag[tag]+"contour"; Hname += lep;
-      hContour[lep][tag] = new TH2F(Hname,Title,nBinsHt, minHt, maxHt, nBinsMet, minMet, maxMet);
+      Hname = Htag[tag]+"contour"; Hname += icut;
+      hContour[icut][tag] = new TH2F(Hname,Title,nBinsHt, minHt, maxHt, nBinsMet, minMet, maxMet);
+      hContour[icut][tag]->SetMarkerStyle(20);
+      hContour[icut][tag]->SetMarkerSize(2.2);
     }      
     for(int sam(0); sam < NSam+1; sam++){
-      Hname = Htag[sam]; Hname += lep;
-      hRate[lep][sam] = new TH2F(Hname,Title+", L = 1.4 #times 10^{34} cm^{-2}s^{-1}",
+      Hname = Htag[sam]; Hname += icut;
+      hRate[icut][sam] = new TH2F(Hname,Title+"          L = 1.4 #times 10^{34} cm^{-2}s^{-1}",
 				 nBinsHt, minHt, maxHt, nBinsMet, minMet, maxMet);
-      hRate[lep][sam]->SetXTitle(xTitle);
-      hRate[lep][sam]->SetYTitle(yTitle);
-      hRate[lep][sam]->SetZTitle(Htag[sam]+" "+zTitle);
-      if(Htag[sam].Contains("sig")) hRate[lep][sam]->SetTitle(Title);
+      hRate[icut][sam]->SetXTitle(xTitle);
+      hRate[icut][sam]->SetYTitle(yTitle);
+      hRate[icut][sam]->SetZTitle(Htag[sam]+" "+zTitle);
+      if(Htag[sam].Contains("sig")) hRate[icut][sam]->SetTitle(Title);
       if(sam==NSam-3) {
 	totCut.ReplaceAll("1.4e-2/19600*weight*(","wl1ht200*(Max$(genmus_pt[0])>0&&");
 	if(isEl) totCut.ReplaceAll("genmus", "genels");
-	hRate[lep][sam]->SetZTitle("T1tttt(1025,625) efficiency (%)");	
+	hRate[icut][sam]->SetZTitle("T1tttt(1025,625) efficiency (%)");	
       }
-      if(sam==NSam-2) hRate[lep][sam]->SetZTitle("T1tttt(1200,800) efficiency (%)");	
-      if(sam==NSam-1) hRate[lep][sam]->SetZTitle("T1tttt(1500,100) efficiency (%)");
+      if(sam==NSam-2) hRate[icut][sam]->SetZTitle("T1tttt(1200,800) efficiency (%)");	
+      if(sam==NSam-1) hRate[icut][sam]->SetZTitle("T1tttt(1500,100) efficiency (%)");
       if(sam<NSam) chain[indchain[sam]]->Project(Hname, "abs(onmet):onht", totCut);
+      // if(sam>=NSam-3 && sam<=NSam-1){
+      // 	hRate[icut][sam]->SetMarkerStyle(20);
+      // 	hRate[icut][sam]->SetMarkerSize(0.5);
+      // 	hRate[icut][sam]->Draw("scat=2");
+      // 	Pname = "plots/hltstudy/scatter_"+tagfolder + "_"+Tags[icut]+ ".eps";
+      // 	can.SaveAs(Pname);
+      // }
+      if(Htag[sam]=="Total") hRate[icut][sam]->SetZTitle("HLT rate [QCD + tt + W#rightarrowl#nu] (Hz)");
     }
 
     // Finding yields for different cuts
@@ -117,66 +142,69 @@ void hltstudy(TString folder="root/hlt/ht200/mu15/", TString filetype = ".eps"){
       for(int metbin(1); metbin <= nBinsMet; metbin++){
 	float bkgrate(0), rate;
 	for(int sam(0); sam < NSam; sam++){
-	  rate = hRate[lep][sam]->Integral(htbin, nBinsHt+1, metbin, nBinsMet+1);
+	  rate = hRate[icut][sam]->Integral(htbin, nBinsHt+1, metbin, nBinsMet+1);
 	  if(sam < NSam-3) bkgrate += rate;
 	  else rate *= (100./(double)noriginal[indchain[sam]]);
-	  hRate[lep][sam]->SetBinContent(htbin, metbin, rate);
+	  hRate[icut][sam]->SetBinContent(htbin, metbin, rate);
 	}
-	hRate[lep][NSam]->SetBinContent(htbin, metbin, bkgrate);
+	hRate[icut][NSam]->SetBinContent(htbin, metbin, bkgrate);
+	hContour[icut][3]->SetBinContent(htbin, metbin, bkgrate);
 	// Filling contour histograms and printing cuts for desired rate levels
-	for(int tag(0); tag < 4; tag++){
+	for(int tag(0); tag < 3; tag++){
+	  hContour[icut][tag]->SetBinContent(htbin, metbin, hRate[icut][NSam-3+tag]->GetBinContent(htbin, metbin));
 	  if(iht < htlevels.size()){
-	    if(hContour[lep][tag]->GetXaxis()->GetBinLowEdge(htbin) >= htlevels[iht]){
+	    if(hContour[icut][tag]->GetXaxis()->GetBinLowEdge(htbin) >= htlevels[iht]){
 	      if(itag==tag && bkgrate<=levels[itag] || metbin==nBinsMet) {
 		if(itag==3) 
-		  cout<<"-- HT > "<<RoundNumber(hContour[lep][tag]->GetXaxis()->GetBinLowEdge(htbin),1)<<endl;
-		cout<<"MET > "<<RoundNumber(hContour[lep][tag]->GetYaxis()->GetBinLowEdge(metbin),1)
+		  cout<<"-- HT > "<<RoundNumber(hContour[icut][tag]->GetXaxis()->GetBinLowEdge(htbin),1)<<endl;
+		cout<<"MET > "<<RoundNumber(hContour[icut][tag]->GetYaxis()->GetBinLowEdge(metbin),1)
 		    <<": \t"<<RoundNumber(bkgrate,2)<<" Hz, \teff1200 "
-		    <<RoundNumber(hRate[lep][NSam-2]->GetBinContent(htbin, metbin),1)<<" %, \teff1500 "
-		    <<RoundNumber(hRate[lep][NSam-1]->GetBinContent(htbin, metbin),1)<<endl;
+		    <<RoundNumber(hRate[icut][NSam-2]->GetBinContent(htbin, metbin),1)<<" %, \teff1500 "
+		    <<RoundNumber(hRate[icut][NSam-1]->GetBinContent(htbin, metbin),1)<<endl;
 		itag--;
 		if(itag==-1) {iht++; itag=3; cout<<endl;}
 	      }
 	    }
 	  }
-	  hContour[lep][tag]->SetBinContent(htbin, metbin, bkgrate);
 	} // Loop over contour lines
       } // Loop over MET bins
     } // Loop over HT bins
-    hRate[lep][NSam]->Draw("colz");
+    hRate[icut][NSam]->Draw("colz");
     leg.Clear();
     for(int tag(0); tag < 4; tag++){
-      hContour[lep][tag]->SetContour(1,&levels[tag]); hContour[lep][tag]->SetLineColor(lcolors[tag]);
-      hContour[lep][tag]->SetLineWidth(4);
-      hContour[lep][tag]->Draw(styles[tag]);
-      leg.AddEntry(hContour[lep][tag], legtag[tag]);
+      //hContour[icut][tag]->SetContour(1,&levels[tag]); hContour[icut][tag]->SetLineColor(lcolors[tag]);
+      hContour[icut][tag]->SetLineWidth(4);
+      hContour[icut][tag]->Draw(styles[tag]);
+      leg.AddEntry(hContour[icut][tag], legtag[tag]);
     }
     // Plotting rates and efficiencies
-    Pnamebase = "plots/hltstudy/rate2d_"; Pnamebase += (isEl?"el":"mu"); 
-    Pnamebase += filetype;
+    Pnamebase = "plots/hltstudy/rate2d_"+tagfolder + "_"+Tags[icut]+ ".eps";
     for(int sam(0); sam < NSam+1; sam++){
+      if(Htag[sam].Contains("sig1200") ||Htag[sam].Contains("sig1500")) continue;
       Pname = Pnamebase; 
       if(Htag[sam].Contains("sig")) {
 	Pname.ReplaceAll("rate2d", Htag[sam]+"effi2d");
 	can.SetLogz(0);
       } else {
-	hRate[lep][sam]->SetMinimum(0.01);
-	hRate[lep][sam]->SetMaximum(2*hRate[lep][NSam]->GetMaximum());
+	hRate[icut][sam]->SetMinimum(0.01);
+	hRate[icut][sam]->SetMaximum(2*hRate[icut][NSam]->GetMaximum());
 	Pname.ReplaceAll("rate2d", Htag[sam]+"rate2d");
 	can.SetLogz(1);
       }
-      hRate[lep][sam]->Draw("colz");
-      if(sam>=NSam-3) for(int tag(0); tag < 4; tag++) hContour[lep][tag]->Draw(styles[tag]);
-      leg.Draw();
+      hRate[icut][sam]->Draw("colz");
+      if(sam>=NSam-3) {
+	for(int tag(0); tag < 1; tag++) hContour[icut][sam-NSam+3]->Draw("text same");
+	//leg.Draw();
+      }
       can.SaveAs(Pname);
     }
-  } // Loop over lepton cuts
+  } // Loop over cuts
 
-  for(int lep(0); lep < NLeps; lep++){
+  for(int icut(0); icut < NCuts; icut++){
      for(int tag(0); tag < NSam+1; tag++)
-       if(hRate[lep][tag]) hRate[lep][tag]->Delete();
+       if(hRate[icut][tag]) hRate[icut][tag]->Delete();
      for(int tag(0); tag < 4; tag++)
-       if(hContour[lep][tag]) hContour[lep][tag]->Delete();
+       if(hContour[icut][tag]) hContour[icut][tag]->Delete();
   }
 }
 
@@ -270,143 +298,12 @@ TString RatesLine(int ht, int met, int pt, bool domu, TChain *chain[NSam], int n
 }
 
 
-// Not fully adapted to ntuples from HLT babies
-void hltstudy1d(TString folder="root/hlt/conf710/el15", bool owen=true, TString filetype = ".eps"){
-  styles style("Standard"); style.setDefaultStyle();
-  gStyle->SetHatchesLineWidth(2);
- 
-  //Files
-  TString legNames[] = {"T1tttt(825,525)","T1tttt(1025,625)", "t#bar{t}", "QCD", "W + jets"}, legCaption;
-  TChain *chain[NSam];
-  int noriginal[NSam];
-  ReadChains(chain, noriginal, folder);
-  vector<int> indchain;
-  indchain.push_back(3);
-  indchain.push_back(2);
-  if(!owen) indchain.push_back(4);
-
-  // Variables and cuts
-  TString VarName[] = {"onht", "onmet", "onht", "onmet"};
-  //TString Cuts[] = {"onmupt>15", "onmupt>15", "onmupt>15&&ht>0", "onmupt>15&&ht>0"};
-  TString Cuts[] = {"1", "1", "ht>0", "ht>0"};
-  //TString Cuts[] = {"1", "1", "onmupt>15&&ht>0", "onmupt>15&&ht>0"};
-  TString tags[] = {"", "", "ht0", "ht0"};
-  float Range[NVar][2];
-  int nBins[NVar];
-  for(int var(0); var < NVar; var++){
-    if(VarName[var].Contains("ht")){
-      Range[var][0] = 400;
-      Range[var][1] = 1800;
-      nBins[var] = 35;
-    }
-    if(VarName[var].Contains("met")){
-      Range[var][0] = 30;
-      Range[var][1] = 500;
-      nBins[var] = 35;
-      if(owen){
-      Range[var][0] = 80;
-      Range[var][1] = 330;
-      }
-    }
-  }
-
-  // Histograms and canvas
-  TCanvas can;
-  TH1F *hFile[NVar][NSam];
-  TH1F hEntries("hEntries","",100,0,5000);
-  int colors[] = {kRed-7, kGreen+1, 4, kBlue-7, kBlue+1, kGreen+2, kMagenta+2};
-  TString xTitle = "", yTitle = "", Title = "", Pname, Hname, totCut;
-  float maxHisto(-1), entries[NSam];
-  double legX = 0.8, legY = 0.93;
-  double legW = 0.12, legH = 0.2;
-  TLegend leg(legX, legY-legH, legX+legW, legY);
-  leg.SetTextSize(0.056); leg.SetFillColor(0); leg.SetFillStyle(0); leg.SetBorderSize(0);
-  leg.SetTextFont(132);
-  TLine line; line.SetLineColor(2); line.SetLineWidth(2); line.SetLineStyle(2);
-  TArrow arrow; arrow.SetLineWidth(2); arrow.SetArrowSize(.02);
-
-  for(int var(0); var < NVar; var++){
-    double xcut = -999.;
-    xTitle = "";
-    if(VarName[var].Contains("met")) {xTitle = "Minimum offline PF E_{T,miss}"; xcut = 50;}
-    if(VarName[var].Contains("onmet")) {xTitle = "Minimum online PF E_{T,miss}"; xcut = 50;}
-    if(VarName[var].Contains("ht")) {xTitle = "Minimum online PF H_{T}";  xcut = 350;}
-    if(VarName[var].Contains("mupt")) {xTitle = "Cut on muon p_{T}"; xcut = 20;}
-    if(VarName[var].Contains("elpt")) {xTitle = "Cut on electron p_{T}"; xcut = 20;}
-    xTitle+=" (GeV)";
-    yTitle = "HLT rate for L = 1.4 cm^{-2}s^{-1} (Hz)";
-    leg.Clear(); leg.SetX1NDC(legX);leg.SetY2NDC(legY);
-
-    for(unsigned int his(0); his < indchain.size(); his++){
-      //Creating histogram
-      Hname = "histo"; Hname += var; Hname += his;
-      hFile[var][his] = new TH1F(Hname, "", nBins[var], Range[var][0], Range[var][1]);
-      hFile[var][his]->SetLineColor(colors[his]);
-      hFile[var][his]->SetLineWidth(3);
-      hFile[var][his]->Sumw2();
-      Title = Cuts[var]; Title.ReplaceAll("onmupt","p_{T}^{#mu}");
-      Title.ReplaceAll("&&",", "); Title.ReplaceAll("ht","H_{T}^{off}");
-      Title.ReplaceAll(">"," > ");
-      if(Title=="1") Title = "";
-      hFile[var][his]->SetTitle(Title);
-      hFile[var][his]->SetXTitle(xTitle);
-      hFile[var][his]->SetYTitle(yTitle);
-
-      // Finding yields for different cuts
-      totCut = "1.4e-2/19600*weight*(" + VarName[var] + ">"; // Instant lumi 1.4e34 [1/cm2/s]
-      totCut += hFile[var][his]->GetBinLowEdge(1); totCut += "&&";
-      totCut += (Cuts[var]  + ")");
-      chain[indchain[his]]->Project(Hname, VarName[var], totCut);
-      for(int bin(1); bin <= nBins[var]; bin++){
-	hFile[var][his]->SetBinContent(bin, hFile[var][his]->Integral(bin, nBins[var]+1));
-	double w2(0);
-	for(int ebin(bin); ebin <= nBins[var]+1; ebin++)
-	  w2 += pow(hFile[var][his]->GetBinError(ebin),2);
-	hFile[var][his]->SetBinError(bin, sqrt(w2));	
-      }
-	
-      entries[his] = hFile[var][his]->Integral();
-
-      if(maxHisto < hFile[var][his]->GetMaximum()) maxHisto = hFile[var][his]->GetMaximum();
-    } // Loop over samples
-
-    leg.Clear();
-    for(unsigned int his(0); his < indchain.size(); his++){
-      legCaption = legNames[indchain[his]]; 
-      leg.AddEntry(hFile[var][his], legCaption);
-      if(his==0) {
-	hFile[var][his]->Draw();
-      } else hFile[var][his]->Draw("same");
-    }
-    leg.Draw();
-    Pname = "plots/hltstudy/rate_"; Pname += VarName[var]; Pname += tags[var]; 
-    Pname += filetype;
-    Pname.ReplaceAll("[","_"); Pname.ReplaceAll("]","");  Pname.ReplaceAll("+","-"); 
-    can.SetLogy(1);
-    hFile[var][0]->SetMinimum(0.001);
-    hFile[var][0]->SetMaximum(maxHisto*10);
-    if(owen){
-      hFile[var][0]->SetMinimum(0.2);
-      hFile[var][0]->SetMaximum(20000);
-    }
-    can.SaveAs(Pname);
-
-  } // Loop over all variables
-
-
-  for(int var(0); var < NVar; var++){
-    for(unsigned int his(0); his < indchain.size(); his++){
-      if(hFile[var][his]) hFile[var][his]->Delete();
-    }
-  }
-}
-
 
 void ReadChains(TChain *chain[], int entries[], TString folder){
   TString FileNames[] = 
     {folder+"/*T1tttt*1025_*", 
-     folder+"/*T1tttt*1200_*", 
-     folder+"/*T1tttt*1500_*", 
+     folder+"/*T1tttt*1025_*", 
+     folder+"/*T1tttt*1025_*", 
      folder+"/*QCD*",
      folder+"/*TT*",
      folder+"/*WJets*"};
@@ -423,6 +320,7 @@ void ReadChains(TChain *chain[], int entries[], TString folder){
       tglobal.SetBranchAddress("nori_genel0",&nori_genel0);
       tglobal.SetBranchAddress("nori_genmu0",&nori_genmu0);
       tglobal.GetEntry(0);
+
       if(isEl) entries[sam] = nori_genel0;
       else entries[sam] = nori_genmu0;
     }
